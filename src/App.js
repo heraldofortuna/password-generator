5b05fa074e7a00eacae10892879fb2adb2bcfcee
heraldofortuna <heraldofortuna@gmail.com>
2022-09-19 15:20:43 -0500

    Se realizó la composición de carpetas correcta para el proyecto, además de crear la estructura en App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
+import React, { useState } from "react";
+
 import "./App.css";
 
 const App = () => {
-  return <div className="App">App</div>;
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChange = (event) => {
+    const { value } = event.target;
+
+    setCurrentPassword(value);
+  };
+
+  const handleClick = () => {
+    setPassword(currentPassword);
+  };
+
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <header>
+          <div>
+            <h1>Password Generator</h1>
+          </div>
+        </header>
+        <main>
+          <div className="wrapper">
+            <label>
+              Ingrese palabra clave
+              <input onChange={handleChange} />
+            </label>
+            <span id="password">{password}</span>
+            <button onClick={handleClick}>Generar</button>
+          </div>
+        </main>
+        <footer>
+          <div className="wrapper">
+            <p>- Hecho por Heraldo Fortuna -</p>
+          </div>
+        </footer>
+      </div>
+    </div>
+  );
 };
 
 export default App;
